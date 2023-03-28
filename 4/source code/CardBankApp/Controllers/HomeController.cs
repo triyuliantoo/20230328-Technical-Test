774196476ebb8f34211142c74a110e3a3937cb00
DESKTOP-6QB8H9G\triyulianto <triyuliantoo@live.com>
2023-03-28 15:35:11 +0700

    Soal 4

---
+++ 4/source code/CardBankApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using CardBankApp.DBContext;
+using CardBankApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CardBankApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			var bank = CBankDAO.ExecuteDatatable("SELECT * FROM Bank WITH(NOLOCK)");
+
+			return View(bank);
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+    }
+}
---
+++ 4/source code/CardBankApp/DBContext/CBankDAO.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CardBankApp.DBContext
+{
+	public class CBankDAO
+	{
+		public static DataTable ExecuteDatatable(string query)
+		{
+			try
+			{
+				// Get database connection string from appsettings.json
+				IConfigurationRoot configuration = new ConfigurationBuilder()
+					.AddJsonFile("appsettings.json", optional: false)
+					.Build();
+				string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+				// Create database connection and command objects
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				using (SqlCommand command = new SqlCommand(query, connection))
+				{
+					connection.Open();
+
+					// Create DataTable and load data from SqlDataReader
+					DataTable dataTable = new DataTable();
+					using (SqlDataReader reader = command.ExecuteReader())
+					{
+						dataTable.Load(reader);
+					}
+
+					return dataTable;
+				}
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+
+
+		}
+
+	}
+}
